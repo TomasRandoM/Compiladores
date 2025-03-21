230f6945dc58c05ac7d263fac01ecec202e4bc14
Tomas Rando <tomasrando@gmail.com>
2025-03-21 13:30:03 -0300

    Create initial structure

---
+++ Compilador/src/main/java/com/uncuyo/compiladores/exceptions/LexicalException.java
@@ -0,0 +1,11 @@
+package com.uncuyo.compiladores.exceptions;
+
+/**
+ * Exception del analizador léxico
+ * @version 1.0.0
+ */
+public class LexicalException extends Exception {
+    public LexicalException(String message) {
+        super(message);
+    }
+}
