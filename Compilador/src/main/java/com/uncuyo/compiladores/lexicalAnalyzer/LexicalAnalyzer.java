e91f6caa4928af9eda6eac160b8acc8e559dfeb9
paulisuden <spaulinaabril@gmail.com>
2025-03-21 19:10:20 -0300

    primeros metodos

--- Compilador/src/main/java/com/uncuyo/compiladores/lexicalAnalyzer/LexicalAnalyzer.java
+++ Compilador/src/main/java/com/uncuyo/compiladores/lexicalAnalyzer/LexicalAnalyzer.java
@@ -1,5 +1,6 @@
 package com.uncuyo.compiladores.lexicalAnalyzer;
 
+import com.uncuyo.compiladores.exceptions.LexicalException;
 import com.uncuyo.compiladores.exceptions.ReaderException;
 import java.util.HashMap;
 import java.util.List;
@@ -11,8 +12,8 @@ public class LexicalAnalyzer {
 
     private HashMap<String, TokenTypes> keywords;
     private ModifiedFileReader fileReader;
-    private int row;
-    private int column;
+    private int row = 0;
+    private int column = 0;
 
     public LexicalAnalyzer() throws ReaderException {
         keywords = new HashMap<>();
@@ -20,7 +21,7 @@ public class LexicalAnalyzer {
         fileReader = new ModifiedFileReader();
     }
 
-    public Token nextToken(Character caracter) throws ReaderException {
+    public Token nextToken(Character caracter) throws ReaderException, LexicalException {
         Character c = fileReader.readChar();
         while (c != null) {
             c = fileReader.readChar();
@@ -34,7 +35,7 @@ public class LexicalAnalyzer {
             } else if (Character.isDigit(c)) {
                 findNumber(); //entero o decimal
             } else if ( c == '"') {
-                findStringConstant(); // termine "
+                Token token = findStringConstant();
             } else {
                 if (c == ' ') {
                     //
@@ -49,6 +50,79 @@ public class LexicalAnalyzer {
     }
         return null;
     }
+
+    public Token findStringConstant() throws ReaderException, LexicalException {
+        int cont = 0;
+        StringBuilder lexeme = new StringBuilder();
+        Character c = fileReader.readChar();
+        sumRowAndColumn(c);
+        try {
+            while (cont < 1024) {
+                if (c == '"') {
+                    break;
+                }
+                lexeme.append(c);
+                c = fileReader.readChar();
+                sumRowAndColumn(c);
+                cont++;
+            }
+            if (cont == 1024) {
+                throw new LexicalException("Error léxico");
+            }
+            return new Token(TokenTypes.const_string, lexeme.toString(), null, column, row);
+        } catch (ReaderException e) {
+            throw new ReaderException("Error léxico (del Reader)");
+        }
+    }
+
+    public void findComment() throws ReaderException, LexicalException {
+        Character c = fileReader.readChar();
+        if (c == '/') {
+            while (c != '\n') {
+                c = fileReader.readChar();
+            }
+            column = 0;
+            row++;
+            c = fileReader.readChar();
+            nextToken(c);
+        } else if (c == '*') {
+            c = fileReader.readChar();
+            while (c != '*') {
+                c = fileReader.readChar();
+                sumRowAndColumn(c);
+            }
+            c = fileReader.readChar();
+            if (c=='/'){
+                column++;
+                c = fileReader.readChar();
+                sumRowAndColumn(c);
+            } else {
+                throw new LexicalException("Error léxico");
+            }
+        } else {
+            throw new LexicalException("Error léxico");
+        }
+    }
+
+    //funcion para que cuente filas y columnas
+    public void sumRowAndColumn(Character c) {
+        if (c == '\n' || c == '\r') {
+            column = 0;
+            row++;
+        } else if (c == '\t') {
+            column += 4;
+        } else {
+            column++;
+        }
+    }
+
+
+
+
+
+
+
+
 
     private HashMap<String, TokenTypes> initializeHash(HashMap<String, TokenTypes> hash) {
         List<String> plist = List.of(
