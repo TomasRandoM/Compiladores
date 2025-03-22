decb17d9ba9c33ccf0a9fd1a005c7ae2c72e6413
Tomas Rando <tomasrando@gmail.com>
2025-03-22 00:55:28 -0300

    Add first version of findNumber, findIdClass and findIdentifierKeyword. Add utils. Modify ModifiedFileReader

--- Compilador/src/main/java/com/uncuyo/compiladores/lexicalAnalyzer/LexicalAnalyzer.java
+++ Compilador/src/main/java/com/uncuyo/compiladores/lexicalAnalyzer/LexicalAnalyzer.java
@@ -1,6 +1,9 @@
 package com.uncuyo.compiladores.lexicalAnalyzer;
 
+import com.uncuyo.compiladores.exceptions.LexicalException;
 import com.uncuyo.compiladores.exceptions.ReaderException;
+import com.uncuyo.compiladores.utils.Addons;
+
 import java.util.HashMap;
 import java.util.List;
 
@@ -20,24 +23,24 @@ public class LexicalAnalyzer {
         fileReader = new ModifiedFileReader();
     }
 
-    public Token nextToken(Character caracter) throws ReaderException {
+    public Token nextToken(Character caracter) throws ReaderException, LexicalException {
         Character c = fileReader.readChar();
         while (c != null) {
             c = fileReader.readChar();
 
             if ( c == '/') {
                 findComment();
-            } else if (Character.isUpperCase(c)) {
-                findIdClass();
-            } else if (Character.isLowerCase(c)) {
-                findIdentifierKeyword(); //objeto, variable, funcion. Se verifica también si es palabra reservada
+            } else if (Addons.isUpperCase(c)) {
+                findIdClass(c);
+            } else if (Addons.isLowerCase(c)) {
+                findIdentifierKeyword(c); //objeto, variable, funcion. Se verifica también si es palabra reservada
             } else if (Character.isDigit(c)) {
-                findNumber(); //entero o decimal
+                findNumber(c); //entero o decimal
             } else if ( c == '"') {
                 findStringConstant(); // termine "
             } else {
                 if (c == ' ') {
-                    //
+                    column++;
                 }
                 if (c == '\n') {
                     column = 0;
@@ -48,6 +51,129 @@ public class LexicalAnalyzer {
 
     }
         return null;
+    }
+
+    /**
+     * Metodo que identifica keywords o identificadores de objeto, variables o funciones
+     * @param c Primer Character leído en nextToken()
+     * @return Token resultante
+     * @throws ReaderException Excepción causada por la lectura del archivo
+     * @throws LexicalException Excepción causada por errores léxicos.
+     */
+    public Token findIdentifierKeyword(Character c) throws ReaderException, LexicalException {
+        StringBuilder lexeme = new StringBuilder();
+        lexeme.append(c);
+        while (c != null) {
+            c = fileReader.readChar();
+            sumRowAndColumn(c);
+            if (c == null || c == ' ' || c == '\t' || c == ')' || c == '(' || c == ';') {
+                fileReader.unreadChar(c);
+                if (Addons.isLetter(lexeme.charAt(lexeme.length() - 1)) || Character.isDigit(c)) {
+                    if (keywords.get(lexeme.toString()) != null) {
+                        return (new Token(keywords.get(lexeme.toString()), lexeme.toString(), null, row, column));
+                    } else {
+                        keywords.put(lexeme.toString(), TokenTypes.id_obj);
+                        return (new Token(TokenTypes.id_obj, lexeme.toString(), null, row, column));
+                    }
+                }
+            } else {
+                if (c != '\n') {
+                    if (Addons.isLetter(c) || Character.isDigit(c) || c == '_') {
+                        lexeme.append(c);
+                    } else {
+                        throw new LexicalException("Lexical Error in row " + row + ", column " + column +
+                                ". Illegal character: " + c);
+                    }
+                }
+            }
+        }
+        throw new LexicalException("Unexpected end of file at row " + row + ", column " + column);
+    }
+
+    /**
+     * Metodo que identifica identificadores de clase o ciertos keywords (que empiezan con mayuscula)
+     * @param c Primer Character leído en nextToken()
+     * @return Token resultante
+     * @throws ReaderException Excepción causada por la lectura del archivo
+     * @throws LexicalException Excepción causada por errores léxicos.
+     */
+    public Token findIdClass(Character c) throws ReaderException, LexicalException {
+        StringBuilder lexeme = new StringBuilder();
+        lexeme.append(c);
+        while (c != null) {
+            c = fileReader.readChar();
+            sumRowAndColumn(c);
+            if (c == null || c == ' ' || c == '\t' || c == ')' || c == '(' || c == ';') {
+                fileReader.unreadChar(c);
+                if (Addons.isLetter(lexeme.charAt(lexeme.length() - 1))) {
+                    if (keywords.get(lexeme.toString()) != null) {
+                        return (new Token(keywords.get(lexeme.toString()), lexeme.toString(), null, row, column));
+                    } else {
+                        keywords.put(lexeme.toString(), TokenTypes.id_class);
+                        return (new Token(TokenTypes.id_class, lexeme.toString(), null, row, column));
+                    }
+                }
+            } else {
+                if (c != '\n') {
+                    if (Addons.isLetter(c) || Character.isDigit(c) ||c == '_') {
+                        lexeme.append(c);
+                    } else {
+                        throw new LexicalException("Lexical Error in row " + row + ", column " + column +
+                                ". Illegal character: " + c);
+                    }
+                }
+            }
+        }
+        throw new LexicalException("Unexpected end of file at row " + row + ", column " + column);
+    }
+
+
+    /**
+     * Metodo que identifica constantes enteras y doubles
+     * @param c Primer Character leído en nextToken()
+     * @return Token resultante
+     * @throws ReaderException Excepción causada por la lectura del archivo
+     * @throws LexicalException Excepción causada por errores léxicos.
+     */
+    public Token findNumber(Character c) throws ReaderException, LexicalException {
+        StringBuilder lexeme = new StringBuilder();
+        lexeme.append(c);
+        //1 es para identificar enteros y 2 para doubles
+        int tipo = 1;
+        while (c != null) {
+            c = fileReader.readChar();
+            sumRowAndColumn(c);
+            if (c == null || c == ' ' || c == '\t' || c == ')' || c == '(' || c == ';'
+                    || Addons.isArithmeticOperator(c) || Addons.isRelationalOperator(c) || c == '\n') {
+                fileReader.unreadChar(c);
+                char aux = lexeme.charAt(lexeme.length() - 1);
+                if (Addons.isLetter(aux) || Character.isDigit(aux)) {
+                    if (tipo == 1) {
+                        return (new Token(TokenTypes.const_int, lexeme.toString(), Integer.parseInt(lexeme.toString()),
+                                row, column));
+                    } else {
+                        return (new Token(TokenTypes.const_double, lexeme.toString(),
+                                Double.parseDouble(lexeme.toString()), row, column));
+                    }
+
+                }
+            } else {
+                if (Character.isDigit(c) || c == '.') {
+                    if (c == '.') {
+                        if (tipo == 2) {
+                            throw new LexicalException("Lexical Error in row " + row + ", column " + column +
+                                    ". Illegal character: " + c);
+                        }
+                        tipo = 2;
+                    }
+                    lexeme.append(c);
+                } else {
+                    throw new LexicalException("Lexical Error in row " + row + ", column " + column +
+                            ". Illegal character: " + c);
+                }
+            }
+        }
+        throw new LexicalException("Unexpected end of file at row " + row + ", column " + column);
     }
 
     private HashMap<String, TokenTypes> initializeHash(HashMap<String, TokenTypes> hash) {
--- Compilador/src/main/java/com/uncuyo/compiladores/lexicalAnalyzer/ModifiedFileReader.java
+++ Compilador/src/main/java/com/uncuyo/compiladores/lexicalAnalyzer/ModifiedFileReader.java
@@ -5,18 +5,24 @@ import com.uncuyo.compiladores.exceptions.ReaderException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PushbackReader;
 
 public class ModifiedFileReader {
-    private FileReader fileReader;
+    private PushbackReader fileReader;
 
     public ModifiedFileReader() throws ReaderException {
         try {
-            this.fileReader = new FileReader("entrada.txt");
+            this.fileReader =  new PushbackReader(new FileReader("entrada.txt"), 1);
         } catch (FileNotFoundException e) {
             throw new ReaderException("File not found. Please try again.");
         }
     }
 
+    /**
+     * Lee un caracter y lo devuelve
+     * @return Character
+     * @throws ReaderException Exception para errores de lectura
+     */
     public Character readChar() throws ReaderException {
         int e;
         try {
@@ -28,6 +34,19 @@ public class ModifiedFileReader {
             }
         } catch (IOException ex) {
             throw new ReaderException("Error in reading char");
+        }
+    }
+
+    /**
+     * Nos permite deshacer la lectura de un caracter
+     * @param c Caracter leido anteriormente
+     * @throws ReaderException Exception por si hubiese algun error
+     */
+    public void unreadChar(char c) throws ReaderException {
+        try {
+            fileReader.unread(c);
+        } catch (IOException ex) {
+            throw new ReaderException("Error in unreading char");
         }
     }
 
