329a7885d8eb543770664d50d43c016365b911df
Tomás Rando <tomasrando@gmail.com>
2025-11-20 22:11:51 -0300

    Add new tests. Add JUnit tests

---
+++ Compilador/tests/sintactico/test10.s
@@ -0,0 +1,6 @@
+//Correcto
+start {
+ Fibonacci fib;
+ Int n;
+ fib=newFibonacci().testAcceso().testAcceso2().testAcceso3().testAcceso4;
+ }
---
+++ Compilador/tests/sintactico/test11.s
@@ -0,0 +1,6 @@
+//Incorrecto. Se accede a una clase en la linea 5
+start {
+ Fibonacci fib;
+ Int n;
+ fib=newFibonacci().testAcceso().testAcceso2().testAcceso3().TestAcceso;
+ }
